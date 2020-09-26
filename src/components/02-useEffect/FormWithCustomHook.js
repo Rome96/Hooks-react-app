e5447fb51902103855939f8ac0027ac3ffcc55ae
Turiano Romero <romero.guzman18@gmail.com>
2020-09-26 14:19:58 -0500

    form with custom Hook

---
+++ src/components/02-useEffect/FormWithCustomHook.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useForm } from '../hooks/useForm';
+import './effect.css';
+
+const FormWithCustomHook = () => {
+
+  const [formValues, handleInputChange] = useForm({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const { name, email, password } = formValues;
+
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    if (name === '' || email === '' || password === '') {
+      alert('Todos los campos son obligatorios!')
+    } else {
+      console.log(formValues)
+    }
+    return;
+  }
+
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <h3>Form With Custom Hook</h3>
+      <hr />
+      <div className='form-group'>
+        <label>Name:</label>
+        <input
+          name='name'
+          type='text'
+          value={name}
+          placeholder='name'
+          className='form-control'
+          onChange={(e) => handleInputChange(e)}
+        />
+      </div>
+      <div className='form-group'>
+        <label>Email:</label>
+        <input
+          name='email'
+          type='text'
+          value={email}
+          placeholder='email'
+          className='form-control'
+          onChange={(e) => handleInputChange(e)}
+        />
+      </div>
+      <div className='form-group'>
+        <label>Password:</label>
+        <input
+          name='password'
+          type='password'
+          value={password}
+          placeholder='****'
+          className='form-control'
+          onChange={(e) => handleInputChange(e)}
+        />
+      </div>
+      <button type='submit' className='btn btn-primary'>Send</button>
+    </form>
+  );
+};
+
+export default FormWithCustomHook;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SimpleForm from './components/02-useEffect/SimpleForm';
-// import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
-// import CounterApp from './components/01-useState/CounterApp';
 // import AppHook from './AppHook';
+// import CounterApp from './components/01-useState/CounterApp';
+// import SimpleForm from './components/02-useEffect/SimpleForm';
+import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
+// import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 
-ReactDOM.render(<SimpleForm />, document.getElementById("root"));
+ReactDOM.render(<FormWithCustomHook />, document.getElementById("root"));
 
