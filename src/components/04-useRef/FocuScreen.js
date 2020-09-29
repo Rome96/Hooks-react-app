d1754a41c061993311466f5de56a812acc1e8be4
Turiano Romero <romero.guzman18@gmail.com>
2020-09-29 00:11:08 -0500

    useRef - first use

--- src/components/04-useRef/FocuScreen.js
+++ src/components/04-useRef/FocuScreen.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './useRef.css';
+
 const FocuScreen = () => {
+
+  const inputRef = useRef();
+
+  
+  const handleClick = () => {
+    // document.querySelector('input').select();
+    console.log(inputRef);
+    inputRef.current.select();
+  };
+
   return (
     <>
       <h3>Focus Secreen</h3>
-      <hr/> 
-      <input
-        className='form-control'
-        placeholder='Su nombre'
-      />
-      <button
-        className='btn btn-primary mt-5'
-      >
+      <hr />
+      <input ref={inputRef} className="form-control" placeholder="Su nombre" />
+      <button onClick={handleClick} className="btn btn-outline-primary mt-5">
         Focus
       </button>
     </>
-  )
-}
+  );
+};
 
-export default FocuScreen
+export default FocuScreen;
