2707e2eb1e5b540b8c152737e98f647c1f16b5b6
Turiano Romero <romero.guzman18@gmail.com>
2020-10-04 13:32:11 -0500

    maquetacion TODOs list

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -2,11 +2,18 @@ import React, { useReducer } from 'react';
 import { todoReducer } from './todoReducer';
 import './styles.css';
 
-const initialState = [{
+const initialState = [
+  {
   id: 1,
   desc: 'Learning react',
   done: false
-}]
+ },
+ {
+  id: 2,
+  desc: 'Learning Hooks',
+  done: false
+ }
+]
 
 const TodoApp = () => {
 
@@ -16,13 +23,40 @@ const TodoApp = () => {
 
   return (
     <div>
-      <h3>Todo App</h3>
-      <hr/>
-      <ul>
-        <li>hola</li>
-        <li>humanos</li>
-        <li>jojo</li>
-      </ul>
+      <h3>
+        Todo App <small>({todos.length})</small>
+      </h3>
+      <hr />
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, i) => (
+              <li key={todo.id} className="list-group-item">
+                <p className="text-center">
+                  {i + 1}. {todo.desc}
+                </p>
+                <button className="btn btn-danger">delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Add TODO</h4>
+          <hr/>
+          <form>
+              <input
+                type="text"
+                name="description"
+                autoComplete="off"
+                placeholder="Learning..."
+                className="form-control"
+              />
+              <button className="btn btn-outline-primary mt-2 btn-block">
+                Add
+              </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
