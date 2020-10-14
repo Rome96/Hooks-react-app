1a9dc1bebcadec4e248a72274ee39ee8364e1ba1
Turiano Romero <romero.guzman18@gmail.com>
2020-10-13 21:18:15 -0500

    testing to the fn, increment, decrement, reset, of useCounter

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { useCounter } from '../../hooks/useCounter'
 
 describe('testing hook useCounter', () => {
@@ -10,7 +10,6 @@ describe('testing hook useCounter', () => {
     const increment = result.current.increment;
     const decrement = result.current.decrement;
     const reset = result.current.reset;
-
     expect(counter).toBe(0)
     expect(typeof increment).toBe('function')
     expect(typeof decrement).toBe('function');
@@ -20,7 +19,44 @@ describe('testing hook useCounter', () => {
   test('return counter in 10', () => {
     const { result } = renderHook(() => useCounter(10));
     const counter = result.current.counter
-    
     expect(counter).toBe(10)
-  })
+  });
+
+  test('testing fn increment counter in 1', () => {
+    const { result } = renderHook(() => useCounter(10));
+    const { increment } = result.current;
+
+    act(() => {
+      increment();
+    });
+
+    const { counter } = result.current;
+    expect(counter).toBe(11)
+  });
+
+  test("testing fn decrement counter in 1", () => {
+    const { result } = renderHook(() => useCounter(10));
+    const { decrement } = result.current;
+
+    act(() => {
+      decrement();
+    });
+
+    const { counter } = result.current;
+    expect(counter).toBe(9);
+  });
+
+  test("testing fn reset counter = 0", () => {
+    const { result } = renderHook(() => useCounter(10));
+    const { reset, increment } = result.current;
+
+    act(() => {
+      increment()
+      reset();
+    });
+
+    const { counter } = result.current;
+    expect(counter).toBe(10);
+  });
+  
 });
